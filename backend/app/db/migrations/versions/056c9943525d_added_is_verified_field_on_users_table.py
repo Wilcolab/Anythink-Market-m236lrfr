1076c6aa6c621d6d215df4c25614e4221120d4e9
Felipe Panegalli <felipe.panegalli@gmail.com>
2025-10-18 18:52:41 +0000

    Add new field on users is_verified

---
+++ backend/app/db/migrations/versions/056c9943525d_added_is_verified_field_on_users_table.py
@@ -0,0 +1,23 @@
+"""Added is_verified field on users table
+
+Revision ID: 056c9943525d
+Revises: fdf8821871d7
+Create Date: 2025-10-18 18:51:33.197627
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '056c9943525d'
+down_revision = 'fdf8821871d7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
